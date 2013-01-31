18e1434fe277656f94a2925b8e8e103346b0e2b6
Ali Hamidi <ahamidi@me.com>
2013-01-31 15:15:42 -0800

    Attempt name events sensibly

--- analytics.lua
+++ analytics.lua
@@ -19,22 +19,22 @@
 
 -- Setup
 local headers = {["Content-Type"]="application/json"}
-local url = ""
-local auth = {}
 
 local function init(url, key, password)
 
-	url = url
-	auth = {key, password}
+	storage.stats_url = url
+	storage.stats_key = key
+	storage.stats_password = password
 
 end
 
 local function track(event)
 	lease.acquire(event)
 	if (storage[event]) then
-		storage[event] = storage[event] + 1
+		local num = json.parse(storage[event].count)
+		storage[event] = json.stringify({type="event", name=event, count=(num + 1)})
 	else
-		storage[event] = 1
+		storage[event] = json.stringify({type="event", name=event, count=1})
 	end
 	lease.release(event)
 end
@@ -43,7 +43,35 @@ local function upload()
 	-- TODO:
 	-- * Upload events count to DB
 	-- * Reset events count
-	print("DB URL: "..url)
+
+	local docs = {}
+	local data = {docs=docs}
+
+	-- Insert current time
+	local cur_time = os.time()
+
+	-- Iterate over all stats records in storage and add to params
+	for i=1, #storage do
+		if (json.parse(storage[i]).type == "event") do
+			local event = json.parse(storage[i])
+			table.insert(docs, {
+				event=event.name,
+				time=cur_time,
+				count=event.count
+			})
+		else
+			-- Not an event
+		end
+	end
+
+	local response = http.request {
+		url = storage.stats_url.."/_bulk_docs",
+		method = "POST",
+		headers = {["Content-Type"]="application/json"},
+		auth = {storage.stats_key,storage.stats_password},
+		data = data
+	}
+
 end
 
 return {track=track, init=init, upload=upload}
