dfb604025e4cd20a29937b40b47453090529ee97
Ali Hamidi <ahamidi@me.com>
2013-01-31 13:59:38 -0800

    removed local

--- analytics.lua
+++ analytics.lua
@@ -22,14 +22,14 @@ local headers = {["Content-Type"]="application/json"}
 local url = ""
 local auth = {}
 
-local function init(url, key, password)
+function init(url, key, password)
 
 	url = url
 	auth = {key, password}
 
 end
 
-local function track()
+function track()
 	local path = request.path
 	lease.acquire(path)
 	if (storage[path]) then
