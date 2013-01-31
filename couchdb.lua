daabcfde959c6987b78f75904aa80f14778de328
Ali Hamidi <ahamidi@me.com>
2013-01-30 16:17:55 -0800

    Added POST, PUT and DELETE

--- couchdb.lua
+++ couchdb.lua
@@ -5,7 +5,6 @@ local auth = {storage.db_key, storage.db_password}
 
 -- GET Request
 local get = function(path, params)
-
 	return http.request {
 		url = storage.db_url..storage.db_name.."/"..path,
 		method = "GET",
@@ -16,18 +15,36 @@ local get = function(path, params)
 end
 
 -- POST Request
-local post = function()
-
+local post = function(data)
+	return http.request {
+		url = storage.db_url..storage.db_name.."/",
+		method = "POST",
+		headers = headers,
+		auth = auth,
+		data = data
+	}
 end
 
 -- PUT Request
-local put = function()
-
+local put = function(doc, data)
+	return http.request {
+		url = storage.db_url..storage.db_name.."/"..doc,
+		method = "PUT",
+		headers = headers,
+		auth = auth,
+		data = data
+	}
 end
 
 -- DELETE Request
-local delete = function()
-
+local delete = function(doc, rev)
+		return http.request {
+		url = storage.db_url..storage.db_name.."/"..doc,
+		method = "DELETE",
+		headers = headers,
+		auth = auth,
+		params = {rev=rev}
+	}
 end
 
 return {get=get, post=post, put=put, delete=delete}
